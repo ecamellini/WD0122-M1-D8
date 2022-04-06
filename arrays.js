06323cea16b60d156312db2a04dd5db12ddc5562
Eric Camellini <eric.camellini@gmail.com>
2022-04-06 11:30:17 +0200

    Showing first examples about arrays

---
+++ arrays.js
@@ -0,0 +1,30 @@
+console.log("----------------ARRAYS/LISTS------------------\n")
+
+// We want to represent a grocery list
+// A grocery list item could be something like "Milk"
+
+let groceryList = [
+  "Milk", "Biscuits", "Sugar"
+] // This is a list of strings
+
+console.log(groceryList)
+
+let multiplesOfFive = [5, 10, 15, 20, 25]
+console.log(multiplesOfFive)
+
+let orders = [
+  { id: "1234", product: "iPhone", quantity: 1},
+  { id: "0912", product: "Paper clips", quantity: 50},
+  { id: "0213", product: "Post it notes", quantity: 100},
+]
+
+console.log("\n---------------KNOW HOW MANY THEY ARE------------------\n")
+
+console.log("How many items does our grocery list contain?")
+console.log(groceryList.length)
+
+console.log("\n---------------ACCESS THE VALUE OF AN ITEM------------------\n")
+
+console.log("\n---------------ADDING AN ITEM------------------\n")
+
+console.log("\n---------------REMOVE AN ITEM------------------\n")
